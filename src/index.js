d323db3ef00ae326306acf4541b1f3b55af0e148
Andrey Sinaf <andre.as2016e@gmail.com>
2020-10-14 22:33:20 +0300

    start test

--- src/index.js
+++ src/index.js
@@ -2,5 +2,19 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  if (Array.isArray(matrix) && matrix.length) {
+    let matrixNew = [];
+    let item = [];
+    
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 === 0) { // если четный элемент матрицы, который надо достать и записать в новую
+        matrixNew = matrixNew.concat(matrix[i]); 
+      } else { // если нечетный элемент исходной матрицы, который надо перевернуть
+        item = matrix[i].reverse();
+        matrixNew = matrixNew.concat(item);
+      };
+    };
+    return matrixNew;
+  } else return [];
+  
+};
